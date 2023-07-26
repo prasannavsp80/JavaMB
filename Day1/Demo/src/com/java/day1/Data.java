cc5d69238220ba3bce76d7cd840ac7081f0330d1
PrasananTrainer03 <PrasananTrainer03>
2023-07-26 13:56:12 +0530

    code

---
+++ Day1/Demo/src/com/java/day1/Data.java
@@ -0,0 +1,16 @@
+package com.java.day1;
+
+public class Data {
+
+	public void greeting() {
+		System.out.println("Welcome to Java Programming...");
+	}
+	
+	private void company() {
+		System.out.println("Company is MayBank...");
+	}
+	
+	void timings() {
+		System.out.println("From 9 AM to 7 PM...");
+	}
+}
---
+++ Day1/Demo/src/com/java/day1/Demo.java
@@ -0,0 +1,10 @@
+package com.java.day1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Data obj = new Data();
+		obj.greeting();
+		obj.timings();
+	}
+}
