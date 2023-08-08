34fdf489e3e44bdbb33ae8cb36d9e06c8adc0cde
PrasananTrainer03 <PrasananTrainer03>
2023-08-08 12:57:41 +0530

    code

--- SpringWire2/SpringMain.java
+++ SpringWire2/SpringMain.java
@@ -7,28 +7,10 @@ public class SpringMain {
 
 	public static void main(String[] args) {
 		ApplicationContext  ctx = new ClassPathXmlApplicationContext("spring.xml");
-		
-		EmployeeService serviceByName = ctx.getBean("employeeServiceByName", EmployeeService.class);
-		
-		System.out.println("Autowiring byName. Employee Name="+serviceByName.getEmployee().getName());
-		
-		EmployeeService serviceByType = ctx.getBean("employeeServiceByType", EmployeeService.class);
-		
+		EmployeeService serviceByType = ctx.getBean("employeeServiceByName", EmployeeService.class);
 		System.out.println("Autowiring byType. Employee Name="+serviceByType.getEmployee().getName());
-		
-		EmployeeService serviceByConstructor = ctx.getBean("employeeServiceConstructor", EmployeeService.class);
-		
-		System.out.println("Autowiring by Constructor. Employee Name="+serviceByConstructor.getEmployee().getName());
-		
-		//Testing @Autowired annotations
-		EmployeeAutowiredByTypeService autowiredByTypeService = ctx.getBean("employeeAutowiredByTypeService",EmployeeAutowiredByTypeService.class);
-		
-		System.out.println("@Autowired byType. Employee Name="+autowiredByTypeService.getEmployee().getName());
-
-		EmployeeAutowiredByConstructorService autowiredByConstructorService = ctx.getBean("employeeAutowiredByConstructorService",EmployeeAutowiredByConstructorService.class);
-		
-		System.out.println("@Autowired by Constructor. Employee Name="+autowiredByConstructorService.getEmployee().getName());
+		EmployeeAutoWireByType wireByType = ctx.getBean("employeeAutowiredByTypeService", EmployeeAutoWireByType.class);
+		System.out.println("Autowiring byType. Employee Name="+wireByType.getEmployee().getName());
 
 	}
-
 }
